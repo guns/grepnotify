0ac1ef2d9f08dc8fdd6485995a7baeed9559c7ef
guns <self@sungpae.com>
2016-08-12 23:36:05 -0500

    Integrate notify-send

--- replace.go
+++ replace.go
@@ -5,20 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"regexp"
 	"sync/atomic"
 	"time"
 )
 
-type replacement struct {
-	pat  regexp.Regexp
-	fmt  string
-	args []interface{} // for fmt.Sprintf
-	ch   chan string
-	busy int32
+type notification struct {
+	summary string
+	body    string
 }
 
-var verb = regexp.MustCompile(`%[^%]`)
+type replacement struct {
+	pat        regexp.Regexp
+	fmt        string
+	submatches int
+	args       []interface{} // for fmt.Sprintf
+	ch         chan notification
+	busy       int32
+}
+
+var verbPattern = regexp.MustCompile(`%[^%]`)
 
 func parseReplacements(args []string, opts *options) ([]replacement, error) {
 	if len(args)&1 == 1 {
@@ -32,15 +39,34 @@ func parseReplacements(args []string, opts *options) ([]replacement, error) {
 		p, err := regexp.Compile(args[i])
 		if err != nil {
 			return nil, err
-		} else if len(p.SubexpNames())-1 != len(verb.FindAllString(args[i+1], -1)) {
-			return nil, fmt.Errorf("number of submatches in `%v` does not match number of verbs in `%v`", p, args[i+1])
+		}
+
+		submatches := len(p.SubexpNames()) - 1
+		verbs := len(verbPattern.FindAllString(args[i+1], -1))
+
+		if submatches == 0 && verbs != 0 {
+			return nil, fmt.Errorf("`%v` has no submatches, but `%v` contains format verbs", p, args[i+1])
+		} else if verbs != submatches-1 {
+			s := "es"
+			if submatches == 1 {
+				s = ""
+			}
+			v := "s"
+			if verbs == 1 {
+				v = ""
+			}
+			return nil, fmt.Errorf(
+				"`%v` has %d submatch%s, but `%v` has %d verb%s, not %d",
+				p, submatches, s, args[i+1], verbs, v, submatches-1,
+			)
 		}
 
 		r := &reps[i>>1]
 		r.pat = *p
 		r.fmt = args[i+1]
-		r.args = make([]interface{}, len(p.SubexpNames())-1)
-		r.ch = make(chan string)
+		r.submatches = len(p.SubexpNames()) - 1
+		r.args = make([]interface{}, r.submatches-1)
+		r.ch = make(chan notification)
 		if opts.Delay == 0 {
 			r.busy = -1
 		}
@@ -52,26 +78,34 @@ func parseReplacements(args []string, opts *options) ([]replacement, error) {
 func scanReplacements(reps []replacement, s *bufio.Scanner) {
 	for s.Scan() {
 		line := s.Text()
+		var msg notification
 
 		for i := range reps {
 			r := &reps[i]
 
 			// Try to avoid unnecessary work
 			if r.busy != -1 && atomic.LoadInt32(&r.busy) == 1 {
-				fmt.Fprintln(os.Stderr, "AVOIDING WORK!")
 				continue
 			}
 
-			matches := r.pat.FindStringSubmatch(line)
-			if matches == nil {
-				continue
-			}
+			if r.submatches == 0 {
+				if !r.pat.MatchString(line) {
+					continue
+				}
 
-			for i, m := range matches[1:] {
-				r.args[i] = m
-			}
+				msg = notification{r.fmt, line}
+			} else {
+				matches := r.pat.FindStringSubmatch(line)
+				if matches == nil {
+					continue
+				}
 
-			msg := fmt.Sprintf(r.fmt, r.args...)
+				for i, m := range matches[2:] {
+					r.args[i] = m
+				}
+
+				msg = notification{matches[1], fmt.Sprintf(r.fmt, r.args...)}
+			}
 
 			if r.busy == -1 {
 				r.ch <- msg
@@ -80,7 +114,6 @@ func scanReplacements(reps []replacement, s *bufio.Scanner) {
 				select {
 				case r.ch <- msg:
 				default:
-					fmt.Fprintln(os.Stderr, "NOT BLOCKING!")
 				}
 			}
 
@@ -104,6 +137,8 @@ func notifyReplacement(rep *replacement, delay time.Duration) {
 	}
 }
 
-func notify(s string) {
-	fmt.Println(s)
+func notify(n notification) {
+	if err := exec.Command("notify-send", n.summary, n.body).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
